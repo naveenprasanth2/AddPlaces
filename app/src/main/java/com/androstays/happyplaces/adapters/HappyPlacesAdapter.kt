a65b5a42a127e21d6005f286cb7b874f1ccf5d74
naveenprasanth2 <78147993+naveenprasanth2@users.noreply.github.com>
2022-09-18 16:21:12 +0530

    ChangesTillAdapter

---
+++ app/src/main/java/com/androstays/happyplaces/adapters/HappyPlacesAdapter.kt
@@ -0,0 +1,43 @@
+package com.androstays.happyplaces.adapters
+
+import android.content.Context
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.androstays.happyplaces.databinding.ItemHappyPlaceBinding
+import com.androstays.happyplaces.model.HappyPlacesModel
+
+open class HappyPlacesAdapter(
+    private val context: Context,
+    private val list: ArrayList<HappyPlacesModel>
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    inner class MyViewHolder(binding: ItemHappyPlaceBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        val tvTitle = binding.tvTitle
+        val tvDescription = binding.tvDescription
+        val ivPlaceImage = binding.ivPlaceImage
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return MyViewHolder(
+            ItemHappyPlaceBinding.inflate(
+                LayoutInflater.from(parent.context), parent, false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val model = list[position]
+        if (holder is MyViewHolder) {
+            holder.ivPlaceImage.setImageURI(Uri.parse(model.image))
+            holder.tvTitle.text = model.title
+            holder.tvDescription.text = model.description
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+}
